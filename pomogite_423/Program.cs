aaf0a786fa1515c939e0b5fe137a30843bdb25da
234834 <vllucfs@gmail.com>
2025-09-22 10:05:09 +0300

    lj,fdktyj vty.

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -17,6 +17,9 @@ for (int i = 0; i < n; i++)
     money[i] = int.Parse(stroka[1].Trim());
 }
 
+void menu() 
+{ 
+
 Console.WriteLine("Меню:");
 Console.WriteLine("1 - Вывод данных");
 Console.WriteLine("2 - Статистика");
@@ -26,77 +29,80 @@ Console.WriteLine("5 - Поиск по названию");
 Console.WriteLine("0 - Выход");
 
 int choice = Convert.ToInt32(Console.ReadLine());
-switch (choice)
-{
-    case 1:
-        Console.WriteLine("Вы выбрали 1 опцию - Вывод данных");
-        for (int i = 0; i < n; i++)
-        {
-            Console.WriteLine($"Название: {products[i]}, сумма:{money[i]}");
-        }
-        break;
-    case 2:
-        Console.WriteLine("Вы выбрали 2 опцию - Статистика");
-        if (n > 0)
-        {
-            int sum = 0;
-            int min = money[0];
-            int max = money[0];
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Вы выбрали 1 опцию - Вывод данных");
             for (int i = 0; i < n; i++)
             {
-                sum += money[i];
-                if (money[i] < min) min = money[i];
-                if (money[i] > max) max = money[i];
-
+                Console.WriteLine($"Название: {products[i]}, сумма:{money[i]}");
             }
-            double average = (double)sum / n;
-            Console.WriteLine($"\nСумма: {sum} руб.");
-            Console.WriteLine($"Среднее: {average} руб.");
-            Console.WriteLine($"Максимум: {max} руб.");
-            Console.WriteLine($"Минимум: {min} руб.");
-        }
-        break;
-    case 3:
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - i - 1; j++)
-                if (money[j] > money[j + 1])
-                {
-                    int tempM = money[j];
-                    money[j] = money[j + 1];
-                    money[j + 1] = tempM;
-
-                    string tempN = products[j];
-                    products[j] = products[j + 1];
-                    products[j + 1] = tempN;
-                }
-        Console.WriteLine("Отсортировано!");
-        break;
-    case 4:
-        Console.Write("Курс (рублей за 1 валюту): ");
-        decimal rate = decimal.Parse(Console.ReadLine());
-        Console.Write("Символ валюты: ");
-        string sym = Console.ReadLine();
-
-        Console.WriteLine($"\nВ {sym}:");
-        foreach (decimal m in money)
-            Console.WriteLine($"{m / rate} {sym}");
-        break;
-    case 5:
-        Console.Write("Искать: ");
-        string search = Console.ReadLine().ToLower();
-        bool found = false;
-
-        for (int i = 0; i < n; i++)
-            if (products[i].ToLower().Contains(search))
+            break;
+        case 2:
+            Console.WriteLine("Вы выбрали 2 опцию - Статистика");
+            if (n > 0)
             {
-                Console.WriteLine($"{products[i]} - {money[i]} руб.");
-                found = true;
-            }
+                int sum = 0;
+                int min = money[0];
+                int max = money[0];
+                for (int i = 0; i < n; i++)
+                {
+                    sum += money[i];
+                    if (money[i] < min) min = money[i];
+                    if (money[i] > max) max = money[i];
 
-        if (!found) Console.WriteLine("Не найдено");
-        break;
-    case 0:
-        break;
-        //4
+                }
+                double average = (double)sum / n;
+                Console.WriteLine($"\nСумма: {sum} руб.");
+                Console.WriteLine($"Среднее: {average} руб.");
+                Console.WriteLine($"Максимум: {max} руб.");
+                Console.WriteLine($"Минимум: {min} руб.");
+            }
+            break;
+        case 3:
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (money[j] > money[j + 1])
+                    {
+                        int tempM = money[j];
+                        money[j] = money[j + 1];
+                        money[j + 1] = tempM;
+
+                        string tempN = products[j];
+                        products[j] = products[j + 1];
+                        products[j + 1] = tempN;
+                    }
+            Console.WriteLine("Отсортировано!");
+            break;
+        case 4:
+            Console.Write("Курс (рублей за 1 валюту): ");
+            decimal rate = decimal.Parse(Console.ReadLine());
+            Console.Write("Символ валюты: ");
+            string sym = Console.ReadLine();
+
+            Console.WriteLine($"\nВ {sym}:");
+            foreach (decimal m in money)
+                Console.WriteLine($"{m / rate} {sym}");
+            break;
+        case 5:
+            Console.Write("Искать: ");
+            string search = Console.ReadLine().ToLower();
+            bool found = false;
+
+            for (int i = 0; i < n; i++)
+                if (products[i].ToLower().Contains(search))
+                {
+                    Console.WriteLine($"{products[i]} - {money[i]} руб.");
+                    found = true;
+                }
+
+            if (!found) Console.WriteLine("Не найдено");
+            break;
+        case 0:
+            return;
+            //4
+    }
+    menu();
 }
+menu();
 
