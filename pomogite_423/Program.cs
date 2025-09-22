499643a423bb42ffaa1c3ff868e8fc38fa427e11
234834 <vllucfs@gmail.com>
2025-09-22 12:02:19 +0300

    Добавлено меню, класс и функции

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/*Console.WriteLine("Введите код товара");
+Console.WriteLine("Введите название товара ");
+Console.WriteLine("Введите цену товара: ");
+Console.WriteLine("Количество: ");
+Console.WriteLine("Остался ли товар еще на складе");
+*/
+class Products
+{
+    public int code;
+    public string name;
+    public int price;
+    public int kol;
+    public bool ostatok;
+
+    
+    public void add(int code, string mame, int price, int kol,bool ostatok)
+    {
+        
+    }
+
+    public void del()
+    {
+
+    }
+    public void postavka()
+    {
+
+    }
+
+    public void prodazha()
+    {
+
+    }
+
+    public void poisk()
+    {
+
+    }
+}
+Console.WriteLine("ПРИЛОЖЕНИЕ ДЛЯ УЧЁТА ТОВАРОВ В МАГАИЗНЕ");
+Console.WriteLine("Что вы хотите сделать?");
+Console.WriteLine("1 - добавить товар");
+Console.WriteLine("2 - удалить товар");
+Console.WriteLine("3 - заказать поставку товара");
+Console.WriteLine("4 - продать товар");
+Console.WriteLine("5 - поиск товаров");
+
+
+
