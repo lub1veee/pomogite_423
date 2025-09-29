42b8b8b03ebddcdbdc32a48eeefd477216c0d8fe
Nasty <vllucfs@gmail.com>
2025-09-30 00:21:33 +0300

    функ2ия для подсчета слов

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -24,3 +24,34 @@ static void Menu()
     Console.WriteLine("2 - Статистика ласт текстов");
     Console.WriteLine("3 - зеенд");
 }
+
+
+static void Anal1z()
+{
+    string Text;
+    Console.WriteLine("Введите текст:");
+    Text = Console.ReadLine();
+    if (Text.Length >= 100) break;
+    Console.WriteLine("Миннимум 100 символов");
+}
+
+static int countw(string text)
+{
+    int count = 0;
+    bool word = false;
+    foreach (char t in text)
+    {
+        if (char.IsLetterOrDigit(t))
+        {
+
+
+            if (!word)
+            {
+                count++;
+                word = true;
+            }
+        }
+        else { word = false; }
+    }
+    return count;
+}
