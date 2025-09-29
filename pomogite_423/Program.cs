c6db41e9793915c47e96d71bb20f6e3c6dbda1ae
Nasty <vllucfs@gmail.com>
2025-09-30 00:11:25 +0300

    добавлено меню, но оно походу не работает :(

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -2,5 +2,25 @@
 
 using System;
 
+Menu();
+var choice = Console.ReadLine();
+while (true)
+{
+    switch (choice)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            return;
+    }
+}
 
-
+static void Menu()
+{
+    Console.WriteLine("Что вы хотите сделать?");
+    Console.WriteLine("1 - Анализ текста");
+    Console.WriteLine("2 - Статистика ласт текстов");
+    Console.WriteLine("3 - зеенд");
+}
