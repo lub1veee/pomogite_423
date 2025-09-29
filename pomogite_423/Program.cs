fa43d4e878314ca7fbac86d0b632d3207de581a7
Nasty <vllucfs@gmail.com>
2025-09-30 01:53:51 +0300

    я доделала, но по-моему я немного сломала свой код, ЧТО ПРОИСХОДИТ

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -2,80 +2,96 @@
 
 using System;
 
-Menu();
-var choice = Console.ReadLine();
-while (true)
-{
-    switch (choice)
+class Analiz
+{ 
+    static List<string> all = new List<string>();
+    static void Main()
     {
-        case "1":
-            break;
-        case "2":
-            break;
-        case "3":
-            return;
+        while (true)
+        {
+            Console.WriteLine("Что вы хотите сделать?");
+            Console.WriteLine("1 - Анализ текста");
+            Console.WriteLine("2 - Статистика ласт текстов");
+            Console.WriteLine("3 - зеенд");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Anal1z();
+                    break;
+                case "2":
+                    history();
+                    break;
+                case "3":
+                    return;
+            }
+        }
     }
-}
-
-static void Menu()
-{
-    Console.WriteLine("Что вы хотите сделать?");
-    Console.WriteLine("1 - Анализ текста");
-    Console.WriteLine("2 - Статистика ласт текстов");
-    Console.WriteLine("3 - зеенд");
-}
-
-
 static void Anal1z()
 {
     string Text;
+    while (true)
+    {
     Console.WriteLine("Введите текст:");
     Text = Console.ReadLine();
-    if (Text.Length >= 100) break;
-    Console.WriteLine("Миннимум 100 символов");
-}
+    if (Text != null && Text.Length >= 100) break;
+     Console.WriteLine("Миннимум 100 символов");
 
-static int countw(string text)
-{
-    int count = 0;
-    bool word = false;
-    foreach (char t in text)
-    {
-        if (char.IsLetterOrDigit(t))
-        {
-
-
-            if (!word)
-            {
-                count++;
-                word = true;
-            }
-        }
-        else { word = false; }
     }
-    return count;
+    int word = countw(Text);
+    string korotkii = findWord(Text, true);
+    string dlinii = findWord(Text, false);
+    int predlzh = predlozh(Text);
+    int glas = countGlas(Text);
+    int sogl = countSogl(Text);
+    string stat = getStat(Text);
+
+    string result = $" Слов:{word}  Предложений:{predlzh}  Гласных:{glas}  Согласных: {sogl}  Короткое слоов:{korotkii}  Длинное:{dlinii}  Статистика: {stat}";
+    all.Add(result);
+    Console.WriteLine(result);
+
 }
 
-static string korotkii(string text, bool findShortest)
-{
-    string result = "";
-    string current = "";
-
-    foreach (char c in text)
+    static int countw(string text)
     {
-        if (char.IsLetterOrDigit(c))
+        int count = 0;
+        bool word = false;
+        foreach (char t in text)
         {
-            current += c;
-        }
-        else
-        {
-            if (current.Length > 0)
+            if (char.IsLetterOrDigit(t))
             {
-                if (result.Length == 0) result = current;
-                else if (findShortest && current.Length < result.Length) result = current;
-                else if (!findShortest && current.Length > result.Length) result = current;
+                if (!word)
+                {
+                    count++;
+                    word = true;
+                }
             }
-            current = "";
+            else { word = false; }
+        }
+        return count;
+    }
+
+    static string findWord(string text, bool findShortest)
+    {
+        string result = "";
+        string current = "";
+
+        foreach (char c in text)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                current += c;
+            }
+            else
+            {
+                if (current.Length > 0)
+                {
+                    if (result.Length == 0) result = current;
+                    else if (findShortest && current.Length < result.Length) result = current;
+                    else if (!findShortest && current.Length > result.Length) result = current;
+                }
+                current = "";
+            }
         }
         if (current.Length > 0)
         {
@@ -84,70 +100,83 @@ static string korotkii(string text, bool findShortest)
             else if (!findShortest && current.Length > result.Length) result = current;
         }
 
-        return result.Length > 0 ? result : "нет";
+            return result.Length > 0 ? result : "нет";
     }
-}
-static int predlozh(string text)
-{
-    int count = 0;
-    bool predlzh = false;
-    foreach (var  p in text)
+    static int predlozh(string text)
     {
-        if (char.IsLetterOrDigit(p))
+        int count = 0;
+        bool predlzh = false;
+        foreach (var  p in text)
         {
-            predlzh = true;
-        }
-        else if ((p == '.' || p == '!' || p == '?') && predlzh)
-        {
-            count++;
-            predlzh = false;
-        }
+            if (char.IsLetterOrDigit(p))
+            {
+               predlzh = true;
+            }
+            else if ((p == '.' || p == '!' || p == '?') && predlzh)
+            {
+                count++;
+                predlzh = false;
+            }
     }
     if (predlzh) count++;
     return count;
-}
-
-static int glas(string text)
-{
-    int count = 0;
-    string glas = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
-    foreach (var g in text)
-    {
-        if(glas.IndexOf(g)>= 0) count++;
     }
-    return count;
 
-}
-static int sogl(string text)
-{
-    int count = 0;
-
-    foreach(char s in text)
+    static int countGlas(string text)
     {
-        if(char.IsLetter(s) && glas(s.ToString()) == 0) count++;
-    }
-    return count;
-}
-static string stat(string text)
-{
-    Dictionary<char, int> stats = new Dictionary<char, int>();
-
-    foreach(char s in text)
-    {
-        char lower = char.ToLower(s);
-        if(char.IsLetter(lower))
+        int count = 0;
+        string glas = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+        foreach (var g in text)
         {
-            if(stats.ContainsKey(lower)) stats[lower]++;
-            else stats[lower] = 1;
+        if(glas.IndexOf(g)>= 0) count++;
+        }
+        return count;
+        
+    }
+    static int countSogl(string text)
+    {
+        int count = 0;
+
+        foreach(char s in text)
+        {
+            if(char.IsLetter(s) && countGlas(s.ToString()) == 0) count++;
+        }
+        return count;
+    }
+    static string getStat(string text)
+    {
+        Dictionary<char, int> stats = new Dictionary<char, int>();
+
+        foreach(char s in text)
+        {
+            char lower = char.ToLower(s);
+            if(char.IsLetter(lower))
+            {
+                if(stats.ContainsKey(lower)) stats[lower]++;
+                else stats[lower] = 1;
+            }
+        }
+        List<char> letters = new List<char>(stats.Keys);
+        letters.Sort();
+
+        string result = "";
+        foreach(char letter in letters)
+        {
+            result += $" '{letter}': {stats[letter]}";
+        }
+        return result.Length > 0 ? result : " не найдено";
+    }
+    static void history()
+    {
+        if(all.Count == 0)
+        {
+            Console.WriteLine("Истории неи");
+            return;
+        }
+
+        for (int i = 0; i < all.Count; i++)
+        {
+            Console.WriteLine (all[i]);
         }
     }
-    List<char> letters = new List<char>(stats.Keys);
-    letters.Sort();
-
-    string result = "";
-    foreach(char letter in letters)
-    {
-        result += $" '{letter}': {stats[letter]}";
-    }
-    return result.Length > 0 ? result : " не найдено";
 }
