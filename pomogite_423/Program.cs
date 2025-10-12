ea106c2151b9cbb75da683c6db6ea0b03db09f32
Nasty <vllucfs@gmail.com>
2025-10-13 01:57:26 +0300

    добавлены жанры, класс книг и их выввод

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Genre
+{
+    Fantasy,
+    Romance,
+    Mystery,
+    Thriller,
+    ScienceFictio
+}
+
+public class Book
+{
+    private static int _nextId = 1;
+
+    public int Id { get; private set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public Genre Genre { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+
+    public Book(string title, string author, Genre genre, int year, decimal price)
+    {
+        Id = _nextId++;
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Название: {Title}, Автор: {Author}, Жанр: {Genre}, Год: {Year}, Цена: {Price:C}";
+    }
