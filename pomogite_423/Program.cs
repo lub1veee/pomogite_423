f037cb8a5fd6de6516dd8c5dd2768430d76a4f42
Nasty <vllucfs@gmail.com>
2025-10-13 02:11:54 +0300

    добавлена менюшка и все ее фичи

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 
 public enum Genre
@@ -63,6 +64,32 @@ public class Book
     {
         return $"ID: {Id}, Название: \"{Title}\", Автор: {Author}, Жанр: {Genre}, Год: {Year}, Цена: {Price:C}";
     }
+}
+
+public class Library
+{
+    private List<Book> _books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        _books.Add(book);
+    }
+
+    public bool RemoveBook(int id)
+    {
+        var book = _books.FirstOrDefault(b => b.Id == id);
+        if (book != null)
+        {
+            _books.Remove(book);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Book> GetAllBooks()
+    {
+        return new List<Book>(_books);
+    }
     public void InitializeTestData()
     {
         AddBook(new Book("Лисья Нора", "Нора Сакавич", Genre.Fantasy, 2020, 550));
@@ -70,5 +97,80 @@ public class Book
         AddBook(new Book("Спеши любить", "Николас Спракс", Genre.Romance, 2019, 890));
         AddBook(new Book("Гордость и предубеждение", "Джейн Остин", Genre.Romance, 1813, 720m));
         AddBook(new Book("1984", "Джордж Оруэлл", Genre.ScienceFictio, 1949, 850));
+    }
+}
+class programm
+{
+
+
+    private static Library _library = new Library();
+
+    static void Main(string[] args)
+    {
+        _library.InitializeTestData();
+
+        while (true)
+        {
+            Console.Clear();
+            Menu();
+            var choice = GetUserChoice();
+            ProcessChoice(choice);
+        }
+    }
+    static void Menu()
+    {
+        Console.WriteLine("СИСТЕМА УПРАВЛЕНИЯ БИБЛИОТЕКОЙ");
+        Console.WriteLine("1. Добавить книгу");
+        Console.WriteLine("2. Удалить книгу по ID");
+        Console.WriteLine("3. Найти книги");
+        Console.WriteLine("4. Сортировать книги");
+        Console.WriteLine("5. Самая дорогая и дешёвая книга");
+        Console.WriteLine("6. Группировка книг по авторам");
+        Console.WriteLine("7. Показать все книги");
+        Console.WriteLine("0. Выход");
+        Console.Write("Выберите действие: ");
+    }
+    static int GetUserChoice()
+    {
+        if (int.TryParse(Console.ReadLine(), out int choice))
+        {
+            return choice;
+        }
+        return -1;
+    }
+
+    static void ProcessChoice(int choice)
+    {
+        switch (choice)
+        {
+            case 1:
+                AddBook();
+                break;
+            case 2:
+                RemoveBook();
+                break;
+            case 3:
+                SearchBooks();
+                break;
+            case 4:
+                SortBooks();
+                break;
+            case 5:
+                ShowPriceExtremes();
+                break;
+            case 6:
+                GroupBooksByAuthor();
+                break;
+            case 7:
+                ShowAllBooks();
+                break;
+            case 0:
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Неверный выбор! Нажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+                break;
+        }
     }
 }
