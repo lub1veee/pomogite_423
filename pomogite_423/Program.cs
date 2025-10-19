dfe510374ab84fbd40bc39c2a9ec5eb22795ec61
Nasty <vllucfs@gmail.com>
2025-10-20 02:10:19 +0300

    меня научили немного шарпам

--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Zhttps://github.com/lub1veee/pomogite_423.git
+
+using System.Runtime.CompilerServices;
+using System;
+
+/*internal class Dog
+{
+    public void Move()
+    {
+        Console.WriteLine("Вариант 1");
+    }
+    public void Move(bool v)
+    {
+        Console.WriteLine("Вариант 2");
+    }
+    public void Move(int a)
+    {
+        Console.WriteLine("Вариант 3");
+    }
+}
+class Program
+{
+    void Main(string[] args)
+    {
+        private Dog dog = new Dog();
+        dog.Move();
+        dog.Move(false);
+        dog.Move(5);
+    }
+}
+*/
+
