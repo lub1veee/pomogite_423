e1e1b2bc8b5e5f02cd0fde7b87a7bb04e3d98a93
234834 <vllucfs@gmail.com>
2025-10-20 11:43:43 +0300

    функция запуска

---
+++ pomogite_423/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace pomogite_423
+{
+    internal class Game
+    {
+        public void StartGame()
+        {
+
+        }
+    }
+}
--- pomogite_423/Program.cs
+++ pomogite_423/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace pomogite_423
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+            Game game = new Game();
+            game.StartGame();
+        }
+    }
+}
