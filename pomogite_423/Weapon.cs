e124ff6d7215c3a9323adf97736b2f92b489cbcc
SmbUNeed <133571383+SmbUNeed@users.noreply.github.com>
2025-10-27 10:10:29 +0300

    классы брони и оружия

---
+++ pomogite_423/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pomogite_423
+{
+    internal class Weapon : Item
+    {
+        public int Damage;
+
+    }
+}
