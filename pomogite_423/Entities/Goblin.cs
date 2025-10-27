509a4800a783c971bdc8811fdbab55c82a9da3ce
SmbUNeed <133571383+SmbUNeed@users.noreply.github.com>
2025-10-27 12:07:31 +0300

    класс гоблин и функции атаки в нем

--- pomogite_423/Entities/Goblin.cs
+++ pomogite_423/Entities/Goblin.cs
@@ -8,6 +8,20 @@ namespace pomogite_423.Entities
 {
     internal class Goblin : Enemy
     {
-        
+        private double _critChance;
+        Random random = new();
+        public Goblin(int hp = 30, int damage = 10, int protection = 7, double critChance = 0.2)
+        {
+            Hp = hp;
+            Damage = damage;
+            Protection = protection;
+            _critChance = critChance;
+        }
+
+        public override void AttackPlayer()
+        {
+            if (random.NextDouble() <= _critChance) Player.Instance.GetDamage(Damage * 2);
+            else base.AttackPlayer();
+        }
     }
 }
---
+++ pomogite_423/Entities/Mage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pomogite_423.Entities
+{
+    internal class Mage : Enemy
+    {
+
+    }
+}
