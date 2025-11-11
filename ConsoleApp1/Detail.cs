f9d6e08a0499f54bd7bec882b8d199471d57e759
234834 <vllucfs@gmail.com>
2025-11-11 11:45:32 +0300

    Декомпозиция, добавлена функция для добавления детали

---
+++ ConsoleApp1/Detail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Detail
+    {
+
+
+
+
+
+
+
+
+
+        public void ShowDetail()
+        {
+
+        }
+
+        public void AddDetail()
+        {
+
+        }
+    }
+}
