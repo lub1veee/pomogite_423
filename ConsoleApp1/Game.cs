70aec935eaa2b8e57955c2ccd10842373eec792d
Nasty <vllucfs@gmail.com>
2025-11-17 02:08:36 +0300

    прописан класс AvtoService

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -10,67 +10,80 @@ namespace ConsoleApp1
     {
         class AvtoService
         {
-            public void StartGame()
+            private Random random = new Random();
+
+            private int _currentTurn;
+
+            private const int FINE = 50000;
+            private const int START_BALANCE = 100000;
+
+            private decimal balance;
+            private decimal Balance
             {
-
+                get => balance;
+                set
+                {
+                    balance = value;
+                    if (balance < 0) LoseGame();
+                }
             }
-
-            private void ChooseMenu(Part part)
-            {
-
-            }
-
-            private void LoseGame()
-            {
-
-            }
-
-            private void PayFine()
-
-
-
-            private void ShowBalance()
-
-
-            public static void WaitForUser()
-
-
-
-              private void ShowOrderMenu()
-
-
-             public void ManageOrders()
-
-
-             private void DeliveOrder(Order order)
-
-
-             private void ClaimOrder(Part part)
-
-
-            private void CancelOrder()
-
-
-            public void CompensateDamage(Part part)
-
-
-            private void RepairDetail(Part part)
-
-
-            private void ShowAllDetailsQuantity()
-
-
-            private void ShowDetailsQuantity(Part part)
-
-
-            private decimal CalculateReplacing(Part part)
-
-
-
-
-
+        public void StartGame()
+        {
 
         }
+
+        private void ChooseMenu(Part part)
+        { 
+
+        }
+
+        private void LoseGame()
+        { 
+
+        }
+
+        private void PayFine()
+
+
+
+        private void ShowBalance()
+
+
+        public static void WaitForUser()
+
+
+
+        private void ShowOrderMenu()
+
+
+        public void ManageOrders()
+
+
+        private void DeliveOrder(Order order)
+
+
+        private void ClaimOrder(Part part)
+
+
+        private void CancelOrder()
+
+
+        public void CompensateDamage(Part part)
+
+
+        private void RepairDetail(Part part)
+
+
+        private void ShowAllDetailsQuantity()
+
+
+        private void ShowDetailsQuantity(Part part)
+
+
+        private decimal CalculateReplacing(Part part)
+
+
+
     }
         
 
