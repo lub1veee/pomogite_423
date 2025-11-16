2cab537c3adcb3b25ae75915a74c3db5f3207625
Nasty <vllucfs@gmail.com>
2025-11-17 02:03:00 +0300

    Декомпозиция пока что закончена хд

--- ConsoleApp1/tables/CustomerHistory.cs
+++ ConsoleApp1/tables/CustomerHistory.cs
@@ -8,5 +8,19 @@ namespace ConsoleApp1.tables
 {
     internal class CustomerHistory
     {
+        public int Id { get; set; }
+
+        public int Day { get; set; }
+
+        public int DetailNeeded { get; set; }
+
+        public decimal RepairPrice { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public decimal Earnings { get; set; }
+
+        public virtual Detail DetailNeededNavigation { get; set; } = null!;
     }
+    \
 }
