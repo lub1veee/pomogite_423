f2d5015a94116a238f47e91ee302b05c2f6630e3
Nasty <vllucfs@gmail.com>
2025-11-17 02:06:25 +0300

    прописан класс Order

--- ConsoleApp1/Order.cs
+++ ConsoleApp1/Order.cs
@@ -8,14 +8,26 @@ namespace ConsoleApp1
 {
     internal class Order
     {
+        private static int LastId = 0;
 
+        public int Id;
+        public Detail detail;
+        public int TurnsToDelive;
+        public int PartQuantity;
 
+        public Order(Detail detail, int quantity)
+        {
+            Id = LastId++;
+            Detail = detail;
+            PartQuantity = quantity;
+            TurnsToDelive = 2;
 
-
+            Console.WriteLine($"Заказ {Id} сформирован!");
+        }
 
         public void GetInfo()
         {
-
+            Console.WriteLine($"{Id}. {Detail}: {PartQuantity} шт.\nБудет доставлено через {TurnsToDelive} д.");
         }
     }
 }
