303367a23278d2ae1e28d793827172f3dabe8adf
Nasty <vllucfs@gmail.com>
2025-11-17 02:05:01 +0300

    прописан класс PurchaseOrder

--- ConsoleApp1/tables/PurchaseOrder.cs
+++ ConsoleApp1/tables/PurchaseOrder.cs
@@ -8,5 +8,18 @@ namespace ConsoleApp1.tables
 {
     internal class PurchaseOrder
     {
+        public int Id { get; set; }
+
+        public int PartId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public int DeliveryIn { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public virtual Detail Detail { get; set; } = null!;
     }
 }
