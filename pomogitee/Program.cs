1aa474b71e283520313966bf224fec231de5bfeb
234834 <vllucfs@gmail.com>
2025-11-18 11:37:14 +0300

    составлен план рыботы

---
+++ pomogitee/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создаем бдшку 3НФ
+//Реализация регистрации пользователя с подтверждение пароля 
+//Просмотр товаров, даже без регистрации 
+//Корзина товаров, где можно заказать один или сразу все предметы
+//Выбор пункта выдачи при заказе
+//Функционал просмотра заказов по дате 
+//Вход уже закодированного(зареганого) пользователя
+
+
+namespace pomogitee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
