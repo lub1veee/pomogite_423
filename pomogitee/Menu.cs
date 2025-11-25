a38147a4b42c6cfcd1ccea13b8c845dcf3b0dbcd
SmbUNeed <133571383+SmbUNeed@users.noreply.github.com>
2025-11-25 14:59:45 +0300

    добавлен класс меню и сделана функция для начального меню

---
+++ pomogitee/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pomogitee
+{
+    internal class Menu
+    {
+        public static void ShowPick(params string[] variants)
+        {
+            int counter = 0;
+            Console.WriteLine("Выберите действие:");
+            foreach (string v in variants)
+            {
+                counter++;
+                Console.WriteLine($"{counter}. {v}");
+            }
+        }
+        public static void Separator()
+        {
+            Console.WriteLine(new string('=', 40));
+        }
+
+        public static void Header(string headername)
+        {
+            Console.Clear();
+            Separator();
+            Console.WriteLine(headername);
+            Separator();
+        }
+
+    }
+}
